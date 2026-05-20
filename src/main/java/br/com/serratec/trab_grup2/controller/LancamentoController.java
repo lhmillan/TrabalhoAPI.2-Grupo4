6d48ee6543cf905edc0b86cc62b3f76e7432284a
NaviCustomize <158621972+NaviCustomize@users.noreply.github.com>
2026-05-20 17:21:51 -0300

    complemento no arquivo do controller

--- src/main/java/br/com/serratec/trab_grup2/controller/LancamentoController.java
+++ src/main/java/br/com/serratec/trab_grup2/controller/LancamentoController.java
@@ -1,5 +1,41 @@
 package br.com.serratec.trab_grup2.controller;
 
-public class LancamentoController {
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.serratec.trab_grup2.model.LancamentoVendas;
+import br.com.serratec.trab_grup2.service.LancamentoService;
+
+@RestController
+@RequestMapping("/lancamentos")
+public class LancamentoController {
+	
+	@Autowired
+	private LancamentoService service;
+	
+	@GetMapping("{id}")
+	public ResponseEntity<LancamentoVendas> listarPorId(@PathVariable Long id){
+		Optional<LancamentoVendas> vendas = service.listarPorId(id);
+		if(vendas.isPresent()) {
+			return ResponseEntity.ok(vendas.get());
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public LancamentoVendas InserirLancamento(@RequestBody LancamentoVendas vendas) {
+		return service.inserirLancamento(vendas);
+	}
+	
 }
--- src/main/java/br/com/serratec/trab_grup2/dto/LancamentoVendasResponseDTO.java
+++ src/main/java/br/com/serratec/trab_grup2/dto/LancamentoVendasResponseDTO.java
@@ -3,27 +3,32 @@ package br.com.serratec.trab_grup2.dto;
 import java.time.LocalDate;
 
 public class LancamentoVendasResponseDTO {
-private LocalDate dataVenda;
-private Double valorVenda;
-private String nomeVendedor;
-public LocalDate getDataVenda() {
-    return dataVenda;
-}
-public void setDataVenda(LocalDate dataVenda) {
-    this.dataVenda = dataVenda;
-}
-public Double getValorVenda() {
-    return valorVenda;
-}
-public void setValorVenda(Double valorVenda) {
-    this.valorVenda = valorVenda;
-}
-public String getNomeVendedor() {
-    return nomeVendedor;
-}
-public void setNomeVendedor(String nomeVendedor) {
-    this.nomeVendedor = nomeVendedor;
-}
+	private LocalDate dataVenda;
+	private Double valorVenda;
+	private String nomeVendedor;
 
+	public LocalDate getDataVenda() {
+		return dataVenda;
+	}
+
+	public void setDataVenda(LocalDate dataVenda) {
+		this.dataVenda = dataVenda;
+	}
+
+	public Double getValorVenda() {
+		return valorVenda;
+	}
+
+	public void setValorVenda(Double valorVenda) {
+		this.valorVenda = valorVenda;
+	}
+
+	public String getNomeVendedor() {
+		return nomeVendedor;
+	}
+
+	public void setNomeVendedor(String nomeVendedor) {
+		this.nomeVendedor = nomeVendedor;
+	}
 
 }
