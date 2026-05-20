52f09402f3839b6d4144bf0d327d4814f31c736a
lhmillan <lhcmdbzfann1@gmail.com>
2026-05-20 16:57:49 -0300

    Criação do service + extends

--- src/main/java/br/com/serratec/trab_grup2/repository/LancamentoRepository.java
+++ src/main/java/br/com/serratec/trab_grup2/repository/LancamentoRepository.java
@@ -1,5 +1,9 @@
 package br.com.serratec.trab_grup2.repository;
 
-public interface LancamentoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.serratec.trab_grup2.model.LancamentoVendas;
+
+public interface LancamentoRepository extends JpaRepository<LancamentoVendas, Long> {
 
 }
--- src/main/java/br/com/serratec/trab_grup2/service/LancamentoService.java
+++ src/main/java/br/com/serratec/trab_grup2/service/LancamentoService.java
@@ -1,5 +1,24 @@
 package br.com.serratec.trab_grup2.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.serratec.trab_grup2.model.LancamentoVendas;
+import br.com.serratec.trab_grup2.repository.LancamentoRepository;
+
+@Service
 public class LancamentoService {
 
+    @Autowired
+    private LancamentoRepository repository;
+
+    public LancamentoVendas inserirLancamento(LancamentoVendas lancamento) {
+        return repository.save(lancamento);
+    }
+
+    public Optional<LancamentoVendas> listarPorId(Long id) {
+        return repository.findById(id);
+    }
 }
